54b4ca6ce32b4f2be5031b53c0e39771e0ca6bc4
SoftDevEmma <SoftDevEmma@DESKTOP-I8IPH9N>
2024-04-10 18:38:13 +0100

    Setting up response DTO and Request DTO in web Project

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public enum Apitype
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}

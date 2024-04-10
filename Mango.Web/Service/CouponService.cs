3d3faf1930c6f98556d079091f5e8a371432eb6b
SoftDevEmma <SoftDevEmma@DESKTOP-I8IPH9N>
2024-04-10 21:53:48 +0100

    Implimentation of ICouponService

---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,45 @@
+﻿using Mango.Web.Models.Dto;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+            
+        }
+
+        public Task<ResponseDto?> CreateCouponsAsync(CouponDto couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> DeleteCouponsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> UpdateCouponsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
